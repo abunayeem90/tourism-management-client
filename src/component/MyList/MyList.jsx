d8c4315c44750d6d9d409d4c6b9feb0c996c42ea
abunayeem90 <naeemahmmed4@gmail.com>
2024-05-02 14:12:37 +0600

    add my list only user can acces which data that user added, other user can't acces data

--- src/component/MyList/MyList.jsx
+++ src/component/MyList/MyList.jsx
@@ -92,7 +92,50 @@ const MyList = () => {
             {/* card */}
 
             
-            
+            <div className="grid gap-8 my-6 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
+                
+                {
+                    userData && 
+
+                    userData?.map(item =>  <div key={item._id} className="card w-full bg-base-100 shadow-xl">
+                    
+                 <figure><img src={item.image} alt="Shoes" /></figure>
+                 <div className="card-body">
+                     <h2 className="card-title"></h2>
+                     <p className="font-semibold text-3xl">Country: {item.countryName}</p>
+                     <p>{item.spotName}</p>
+                     <p className="text-primary">{item.shortDescription}</p>
+                     
+                     <p>Location: {item.location}</p>
+                     <div className="flex ">
+                     <p>Seasonality: {item.seasonality}</p>
+                     <p className="">Average Cost: {item.averageCost }</p>
+                     </div>
+                     <div className="flex ">
+                     <p >Travel Time: {item.travelTime}</p>
+                     <p className="">Visitors: {item.visitors}</p>
+                     </div>
+                     <div className="flex mr-6 gap-8 justify-between">
+                        <Link 
+                        to={`update/${item._id}`}
+                        >
+                        <button className="btn text-xl font-medium bg-secondary" type="button">Update</button>
+                        </Link>
+                        <Link>
+                        <button onClick={() =>  {hanldeDelete(item._id)} } className="btn text-xl font-medium  bg-primary" type="button">Delete</button>
+                        </Link>
+                     </div>
+                    
+                     
+                     
+                     {/* <p>{item.userName}</p>
+                     <p>{item.userEmail}</p> */}
+         
+         
+                 </div>
+             </div>)
+                }
+            </div>
         </div>
     );
 };
