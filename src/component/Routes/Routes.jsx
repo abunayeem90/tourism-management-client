92bed55324834b50c0185b86ede9bacf23bb63a8
abunayeem90 <naeemahmmed4@gmail.com>
2024-05-02 17:51:28 +0600

    Tourists Spots Section added 6 data from mongo databse

--- src/component/Routes/Routes.jsx
+++ src/component/Routes/Routes.jsx
@@ -59,9 +59,10 @@ const router = createBrowserRouter([
       },
       {
         path: '/mylist/update/:id',
-        element: <PrivateRouters>
+        element: 
           <UpdateSpot></UpdateSpot>
-        </PrivateRouters>,
+       ,
+          
         loader: ({ params }) => fetch(`https://tourism-management-server-blond.vercel.app/addSpot/${params.id}`)
 
       },
